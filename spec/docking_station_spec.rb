4cd66e23d049ad7d8d479fd3c828539ca95852fd
Arcie <archiefielding@gmail.com>
2016-01-08 15:58:41 +0800

    Added base docking station & bike methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,1 +1,13 @@
+require 'docking_station'
 
+describe DockingStation do
+
+  it { is_expected.to respond_to :release_bike }
+
+  it 'releases working bikes' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).to be_working
+  end
+
+end
