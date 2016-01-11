eaa206cdf4589e0a4be7e004c84f42a3edeff81d
Arcie <archiefielding@gmail.com>
2016-01-11 14:45:13 +0800

    Can store bikes in docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,19 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
   it 'releases working bikes' do
     docking_station = DockingStation.new
+    docking_station.dock Bike.new
     bike = docking_station.release_bike
     expect(bike).to be_working
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
 end
